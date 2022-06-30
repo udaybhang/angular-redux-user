f5fbb9770f1f2232abbf092ed931e956b69a896e
udaybhang <guptaudaybhan02@gmail.com>
2022-06-30 12:17:24 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { UsersComponent } from './users/users.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component: UsersComponent},
+  {path : 'register', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
